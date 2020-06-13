cf7d3cda0b83954d4b5a05015b9975eb8cda3cd4
Dean Gelbaum <deanscottg@gmail.com>
2020-06-13 16:52:13 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,4 @@
   $statement = "It's Dangerous To Go ALone! Take This."
-  $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 def mario
   phrase = "It's-a me, Mario!"
   # status = 'Thank You Mario! But Our Princess Is In Another Castle!'
@@ -8,7 +7,7 @@ end
 
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts $status
+  puts status
 end
 
 def link 
