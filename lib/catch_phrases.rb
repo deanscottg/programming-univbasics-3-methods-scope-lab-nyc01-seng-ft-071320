fb8f662370f58c5b5e88cc56b29e56e4b1bad720
Dean Gelbaum <deanscottg@gmail.com>
2020-06-13 16:52:50 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,4 +1,4 @@
-  $statement = "It's Dangerous To Go ALone! Take This."
+
 def mario
   phrase = "It's-a me, Mario!"
   # status = 'Thank You Mario! But Our Princess Is In Another Castle!'
@@ -13,8 +13,7 @@ end
 def link 
   statement = "It's Dangerous To Go ALone! Take This."
   puts statement
-  
-end
+  end
 
 def all_phrases 
 puts mario
